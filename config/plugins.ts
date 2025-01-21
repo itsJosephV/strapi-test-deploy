ca77bac2c59402766349c51722623d809bd125d6
Joseph <jvp.2703@gmail.com>
2025-01-21 10:39:50 +0000

    Update AWS S3 upload provider configuration with default values

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,14 +1,14 @@
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
   upload: {
     config: {
       provider: 'aws-s3',
       providerOptions: {
-        accessKeyId: env('R2_ACCESS_KEY_ID'),
-        secretAccessKey: env('R2_SECRET_ACCESS_KEY'),
+        accessKeyId: env('R2_ACCESS_KEY_ID', "0127ba6d366a7a0eec014df47be8912b"),
+        secretAccessKey: env('R2_SECRET_ACCESS_KEY', "d79bbc513685ecd13a2d8b955fd2104aa091884daaba9dd8c939336e90468d2f"),
         region: 'auto',
-        endpoint: env('R2_ENDPOINT'),
+        endpoint: env('R2_ENDPOINT', "https://fdbe95617d90cde42b1b98d84ca8ad31.eu.r2.cloudflarestorage.com"),
         params: {
-          Bucket: env('R2_BUCKET_NAME'),
+          Bucket: env('R2_BUCKET_NAME', "strapi-assets"),
         },
       },
     },
