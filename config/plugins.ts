25d440dfa878c7d78c2efb4ac86a3b76d53ec6a7
Joseph <jvp.2703@gmail.com>
2025-01-21 12:28:49 +0000

    Replace AWS S3 upload provider with Cloudflare R2 and update security middleware configuration

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,19 +1,38 @@
 module.exports = ({ env }) => ({
+  // ...
   upload: {
     config: {
-      provider: 'aws-s3',
+      provider: "strapi-provider-cloudflare-r2",
       providerOptions: {
-        accessKeyId: env('R2_ACCESS_KEY_ID', "0127ba6d366a7a0eec014df47be8912b"),
-        secretAccessKey: env('R2_SECRET_ACCESS_KEY', "d79bbc513685ecd13a2d8b955fd2104aa091884daaba9dd8c939336e90468d2f"),
-        region: 'auto',
-        endpoint: env('R2_ENDPOINT', "https://fdbe95617d90cde42b1b98d84ca8ad31.eu.r2.cloudflarestorage.com"),
+        accessKeyId: env("CF_ACCESS_KEY_ID"),
+        secretAccessKey: env("CF_ACCESS_SECRET"),
+        /**
+         * `https://<ACCOUNT_ID>.r2.cloudflarestorage.com`
+         */
+        endpoint: env("CF_ENDPOINT"),
         params: {
-          Bucket: env('R2_BUCKET_NAME', "strapi-assets"),
+          Bucket: env("CF_BUCKET"),
         },
+        /**
+         * Set this Option to store the CDN URL of your files and not the R2 endpoint URL in your DB.
+         * Can be used in Cloudflare R2 with Domain-Access or Public URL: https://pub-<YOUR_PULIC_BUCKET_ID>.r2.dev
+         * This option is required to upload files larger than 5MB, and is highly recommended to be set.
+         * Check the cloudflare docs for the setup: https://developers.cloudflare.com/r2/data-access/public-buckets/#enable-public-access-for-your-bucket
+         */
+        cloudflarePublicAccessUrl: env("CF_PUBLIC_ACCESS_URL"),
+        /**
+         * Sets if all assets should be uploaded in the root dir regardless the strapi folder.
+         * It is useful because strapi sets folder names with numbers, not by user's input folder name
+         * By default it is false
+         */
+        pool: false,
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
       },
     },
   },
-  logger: console.log
+  // ...
 });
-
-
